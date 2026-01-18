5ef21501cb69fbf9e08b61d76621304adbe239a1
Haroon Nkopa <rethabilejames26@gmail.com>
2026-01-18 12:58:42 +0000

    commited config.py from server to github repo

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     # SQLite database path
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///inventory.db'
+    SQLALCHEMY_DATABASE_URI = (
+	os.environ.get("DATABASE_URI")
+	or "sqlite:///"+ os.path.join(basedir, "inventory.db")
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'devkey')
