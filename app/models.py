01e1d2c74864fdcd4c4953737272e574736de7c7
Haroon-Nkopa <rethabilejames26@gmail.com>
2026-01-29 13:22:16 -0800

    Make product name unique in the database

--- app/models.py
+++ app/models.py
@@ -42,7 +42,7 @@ class Product(db.Model):
     __tablename__ = 'product'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
+    name = db.Column(db.String(120), nullable=False, unique=True)
     category = db.Column(db.String(120))
     
     # Retail price
